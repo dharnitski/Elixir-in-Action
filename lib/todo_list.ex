cf038a75a8e18294c483e0ea27d63a3ec574f6fa
Dmitry Harnitski <dharnitski@gmail.com>
2016-05-29 16:17:01 +0200

    add update_entry

--- lib/todo_list.ex
+++ lib/todo_list.ex
@@ -29,11 +29,11 @@ defmodule ToDoList do
         iex> ToDoList.entries(todo_list, {2013, 12, 19})
         [%{date: {2013, 12, 19}, id: 1, title: "Dentist"}]
 
-        iex> todo_list = ToDoList.new |>
+        iex> ToDoList.new |>
         ...> ToDoList.add_entry("not valid type")
         ** (BadMapError) expected a map, got: "not valid type"
 
-        iex> todo_list = ToDoList.new |>
+        iex> ToDoList.new |>
         ...> ToDoList.add_entry(%{date: {2013, 12, 19}, title: "Dentist"})
         iex> ToDoList.entries("not valid type", {2013, 12, 19})
         ** (FunctionClauseError) no function clause matching in ToDoList.entries/2
@@ -66,6 +66,34 @@ defmodule ToDoList do
     |> Enum.map(fn({_, entry}) ->
          entry
        end)
+  end
+
+  @doc """
+
+    Updates existing entry of list
+
+    ## Examples
+
+        iex> todo_list = ToDoList.new
+        ...> |> ToDoList.add_entry(%{date: {2013, 12, 19}, title: "Dentist"})
+        ...> |> ToDoList.update_entry(1, &Map.put(&1, :date, {2014, 12, 19}))
+        iex> todo_list.entries
+        #HashDict<[{1, %{date: {2014, 12, 19}, id: 1, title: "Dentist"}}]>
+
+  """
+  def update_entry(
+    %ToDoList{entries: entries} = todo_list,
+    entry_id,
+    updater_fun
+  ) do
+    case entries[entry_id] do
+      nil -> todo_list
+
+      old_entry ->
+        new_entry = updater_fun.(old_entry)
+        new_entries = HashDict.put(entries, new_entry.id, new_entry)
+        %ToDoList{todo_list | entries: new_entries}
+    end
   end
 
 end
