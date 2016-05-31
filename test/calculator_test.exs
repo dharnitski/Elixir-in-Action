4422a32151703b91aa645b4040d97a4ede6308fb
Dmitry Harnitski <dharnitski@gmail.com>
2016-05-31 22:14:23 +0200

    test invalid status

--- test/calculator_test.exs
+++ test/calculator_test.exs
@@ -1,5 +1,6 @@
 defmodule CalculatorTest do
   use ExUnit.Case
+  import ExUnit.CaptureIO
 
   test "calculate" do
     calculator_pid = Calculator.start
@@ -10,4 +11,14 @@ defmodule CalculatorTest do
     Calculator.div(calculator_pid, 5)
     assert Calculator.value(calculator_pid) == 3.0
   end
-end  
+
+  test "Invalid Message" do
+    calculator_pid = Calculator.start
+    #cannot capture IO from another process
+    assert capture_io(fn ->
+      send(calculator_pid, {:invalid})
+    end) == ""
+    # == "invalid request {:invalid}"
+
+  end
+end
