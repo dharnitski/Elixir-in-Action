f65c16fac0f0509c605510b8c0bf17b35e188812
Dmitry Harnitski <dharnitski@gmail.com>
2016-05-31 20:48:09 +0200

    test 100 processes

--- test/database_server_test.exs
+++ test/database_server_test.exs
@@ -5,6 +5,23 @@ defmodule DatabaseServerTest do
     DatabaseServer.start
     |> DatabaseServer.run_async("query 1")
     assert DatabaseServer.get_result == "query 1 result"
-
   end
+
+  test "pull 100 servers" do
+    #start 100 servers
+    pool = 1..100
+    |> Enum.map(fn(_) -> DatabaseServer.start end)
+
+    1..5
+    |> Enum.each(fn(query_def) ->
+      #select random process
+      server_pid = Enum.at(pool, :random.uniform(100) - 1)
+      DatabaseServer.run_async(server_pid, query_def)
+    end)
+
+    assert 1..5
+    |> Enum.map(fn(_) -> DatabaseServer.get_result end)
+    |> Enum.sort == ["1 result", "2 result", "3 result", "4 result", "5 result"]
+  end
+
 end
