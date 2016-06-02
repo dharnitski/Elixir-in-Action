f752723caa68ab7e63d976839b48b08c23609b6e
Dmitry Harnitski <dharnitski@gmail.com>
2016-06-02 22:44:50 +0200

    add interface functions

--- lib/key_value_store.ex
+++ lib/key_value_store.ex
@@ -9,16 +9,36 @@ defmodule KeyValueStore do
         :ok
         iex> ServerProcess.call(pid, {:get, :some_key})
         :some_value
+
+        iex> pid = KeyValueStore.start
+        iex> KeyValueStore.put(pid, :some_key, :some_value)
+        :ok
+        iex> KeyValueStore.get(pid, :some_key)
+        :some_value
   """
 
   def init do
     Map.new
   end
 
+  #callback functions invoked in the server process
   def handle_call({:put, key, value}, state) do
     {:ok, Map.put(state, key, value)}
   end
   def handle_call({:get, key}, state) do
     {Map.get(state, key), state}
+  end
+
+  #interface functions run in client process
+  def start do
+    ServerProcess.start(KeyValueStore)
+  end
+
+  def put(pid, key, value) do
+    ServerProcess.call(pid, {:put, key, value})
+  end
+
+  def get(pid, key) do
+    ServerProcess.call(pid, {:get, key})
   end
 end
